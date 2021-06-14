5e62d76f023b9b738a5ef08b3501f6804c998dfd
Filipe Pfluck <filipepfluckdev@gmail.com>
2021-06-13 22:39:54 -0300

    chore: fonts and theme declaration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document from 'next/document'
+
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -27,4 +28,19 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+  
+  render(){
+    return(
+        <Html>
+            <Head>
+              <link rel="preconnect" href="https://fonts.gstatic.com"/>
+              <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet"/>
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
+}
 }
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        //example
+        background: '#333',
+        font: '#FFF'
+    }
+}
